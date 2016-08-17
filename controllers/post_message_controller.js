0c97afa94c914476063b725813bfeef94327b4e6
Kendra Lyndon <kendralyndon@gmail.com>
2016-08-17 11:03:34 -0600

    user can add a message and is redirected to "/" path

--- controllers/post_message_controller.js
+++ controllers/post_message_controller.js
@@ -1,4 +1,10 @@
-app.controller('PostMessageController', function ($scope) {
+app.controller('PostMessageController', function ($scope, MessagesService, $location) {
   $scope.vw = {};
   $scope.vw.success = "Success!!"
+  $scope.add = function(text){
+    var message = {};
+    message.text = text;
+    MessagesService.add(message);
+    $location.path('/');
+  }
 })
